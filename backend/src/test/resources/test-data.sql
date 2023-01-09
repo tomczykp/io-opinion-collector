c2fccfb8c6a83ff01fcf7c9af043169b9c006fb2
Lukasz0104 <lukasz.01.stepien@gmail.com>
2023-01-09 15:44:50 +0100

    Fix tests for OpinionController

---
+++ backend/src/test/resources/test-data.sql
@@ -0,0 +1,108 @@
+
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('8378b753-6d05-454b-8447-efb125846fc7', true, 'admin', false, '$2a$10$xjb0b4oZfvGJkIPd2c7FP.owcLUH0h2bIhnTEIUkr4SpofB/cS4yu', 'ADMIN', 'Admin', 'LOCAL', false);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('66208864-7b61-4e6e-8573-53863bd93b35', true, 'user', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User', 'LOCAL', false);
+
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('12208864-7b61-4e6e-8573-53863bd93b35', true, 'user1', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User1', 'LOCAL', false);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('22208864-7b61-4e6e-8573-53863bd93b35', true, 'user2', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User2', 'LOCAL', false);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('32208864-7b61-4e6e-8573-53863bd93b35', true, 'user3', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User3', 'LOCAL', false);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('42208864-7b61-4e6e-8573-53863bd93b35', true, 'user4', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User4', 'LOCAL', false);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('52208864-7b61-4e6e-8573-53863bd93b35', true, 'user5', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User5', 'LOCAL', false);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('72208864-7b61-4e6e-8573-53863bd93b35', true, 'user6', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User6', 'LOCAL', false);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('82208864-7b61-4e6e-8573-53863bd93b35', true, 'user7', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User7', 'LOCAL', false);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('92208864-7b61-4e6e-8573-53863bd93b35', true, 'user8', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User8', 'LOCAL', false);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('62108864-7b61-4e6e-8573-53863bd93b35', true, 'user9', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User9', 'LOCAL', false);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('62308864-7b61-4e6e-8573-53863bd93b35', true, 'user10', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User10', 'LOCAL', true);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('62408864-7b61-4e6e-8573-53863bd93b35', true, 'user11', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User11', 'LOCAL', false);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('62508864-7b61-4e6e-8573-53863bd93b35', true, 'user12', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User12', 'LOCAL', true);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('62608864-7b61-4e6e-8573-53863bd93b35', true, 'user13', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User13', 'LOCAL', false);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('62708864-7b61-4e6e-8573-53863bd93b35', true, 'user14', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User14', 'LOCAL', true);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('62808864-7b61-4e6e-8573-53863bd93b35', true, 'user15', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User15', 'LOCAL', false);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('62908864-7b61-4e6e-8573-53863bd93b35', true, 'user16', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'User16', 'LOCAL', true);
+INSERT INTO public.app_user (id, active, email, locked, password, role, visible_name, provider, deleted) VALUES ('6ddee1ee-9eba-4222-a031-463a849e1886', true, 'Karok2137', false, '$2a$10$76lL2cTtJS1bt3/97Czo0elebu5Lv0oNZytMBBZzqHPgAgyNcSSQG', 'USER', 'Karol2137', 'LOCAL', false);
+
+INSERT INTO iodb.public.question (question_id, content, date, product_id, author_id)
+VALUES ('8d2f2aaf-1b98-4d87-b7e3-97bacd955bc6', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet consequat dui. In sit amet libero sed orci malesuada consectetur nec quis nibh. Vestibulum porttitor, nisl et posuere cursus?',
+        CURRENT_TIMESTAMP, '4811913c-b953-4856-979b-838488049d07', '66208864-7b61-4e6e-8573-53863bd93b35'),
+       ('35c3863a-2241-4c05-942b-6df9dabdf193', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet consequat dui. In sit amet libero sed orci malesuada consectetur nec quis nibh. Vestibulum porttitor, nisl et posuere cursus?',
+        CURRENT_TIMESTAMP, '4811913c-b953-4856-979b-838488049d07', '66208864-7b61-4e6e-8573-53863bd93b35'),
+       ('95bc5f23-ac70-4c7e-a01f-4d9e92fad63d', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet consequat dui. In sit amet libero sed orci malesuada consectetur nec quis nibh. Vestibulum porttitor, nisl et posuere cursus?',
+        CURRENT_TIMESTAMP, '4811913c-b953-4856-979b-838488049d07', '66208864-7b61-4e6e-8573-53863bd93b35');
+
+INSERT INTO iodb.public.answer (answer_id, content, date, question_id, author_id)
+VALUES ('f04041a1-6314-4afd-91bb-29d016b23b95', 'Yes', CURRENT_TIMESTAMP, '8d2f2aaf-1b98-4d87-b7e3-97bacd955bc6', '8378b753-6d05-454b-8447-efb125846fc7'),
+       ('3cafa9ca-f18c-404a-9c1d-d5d1c853eec5', 'No', CURRENT_TIMESTAMP, '35c3863a-2241-4c05-942b-6df9dabdf193', '8378b753-6d05-454b-8447-efb125846fc7');
+
+INSERT INTO public.field (fieldid, name, type) VALUES ('e65b1f71-f3d8-4e16-ba55-df16a316f051', 'Property1', 'String');
+INSERT INTO public.category (categoryid, name, parent_category_category_id) VALUES ('b293013b-40da-4dde-a997-bce3068636ec', 'Category 1', null);
+INSERT INTO public.category_fields (category_categoryid, fields_fieldid) VALUES ('b293013b-40da-4dde-a997-bce3068636ec', 'e65b1f71-f3d8-4e16-ba55-df16a316f051');
+
+INSERT INTO public.field (fieldid, name, type) VALUES ('8c47ae46-8aa9-11ed-a1eb-0242ac120002', 'keyParent', 'valueParent');
+INSERT INTO public.category (categoryid, name, parent_category_category_id) VALUES ('82d58040-8aa9-11ed-a1eb-0242ac120002', 'Category_3', 'b293013b-40da-4dde-a997-bce3068636ec');
+INSERT INTO public.category_fields (category_categoryid, fields_fieldid) VALUES ('82d58040-8aa9-11ed-a1eb-0242ac120002', '8c47ae46-8aa9-11ed-a1eb-0242ac120002');
+
+INSERT INTO public.field (fieldid, name, type) VALUES ('f93af257-b5f2-4643-a844-5b2a7d75873f', 'keyParent2', 'valueParent2');
+INSERT INTO public.category (categoryid, name, parent_category_category_id) VALUES ('f93af257-b1c2-4643-a844-5b2a7d75873f', 'Category_4', '82d58040-8aa9-11ed-a1eb-0242ac120002');
+
+INSERT INTO public.category_fields (category_categoryid, fields_fieldid) VALUES ('f93af257-b1c2-4643-a844-5b2a7d75873f', 'f93af257-b5f2-4643-a844-5b2a7d75873f');
+
+INSERT INTO public.category (categoryid, name, parent_category_category_id) VALUES ('f1bfd355-4bbe-48a4-9acd-c5e886f49277', 'Category 2', null);
+INSERT INTO public.category (categoryid, name, parent_category_category_id) VALUES ('fb909c3b-8f5a-4a54-be79-f9922bfdf1c8', 'Category 2.1', 'f1bfd355-4bbe-48a4-9acd-c5e886f49277');
+INSERT INTO public.category (categoryid, name, parent_category_category_id) VALUES ('e78a56e4-6b3d-40e8-9eeb-4eceac8fc047', 'Category 2.1.1', 'fb909c3b-8f5a-4a54-be79-f9922bfdf1c8');
+
+
+INSERT INTO iodb.public.product (product_id, category_id, constant_product_id,  name, description, deleted, confirmed, created_at)
+VALUES ('4811913c-b953-4856-979b-838488049d07', 'f93af257-b1c2-4643-a844-5b2a7d75873f', 'a64b3ef4-9dee-4088-8e99-bf7e7904d529', 'Product1', 'Description1', false, true, CURRENT_TIMESTAMP),
+       ('cbee1685-9b16-47ae-8c34-afa94e4f1a8f', '82d58040-8aa9-11ed-a1eb-0242ac120002', 'a675ee5e-57da-423b-9a69-dcd7a40a77af', 'Product2', 'Description2', false, true, CURRENT_TIMESTAMP),
+       ('2f335bfb-6805-4a40-ae9c-53b43f22b3ce', 'f93af257-b1c2-4643-a844-5b2a7d75873f', '97ccb8f6-1f17-450e-9c77-f1f123b0d849', 'Product3', 'Description3', false, true, CURRENT_TIMESTAMP),
+       ('b71a95ef-3f19-4ced-a0f4-ebdca10a7668', 'b293013b-40da-4dde-a997-bce3068636ec', 'bad729e9-10e8-4fc6-96c6-6e0f82b781e6', 'Product4', 'Description4', false, true, CURRENT_TIMESTAMP),
+       ('4950d349-1127-4690-82a0-94fdc81b019b', 'b293013b-40da-4dde-a997-bce3068636ec', '4dfd650a-b331-4076-8e26-66d9901c13fb', 'Product5', 'Description5', false, true, CURRENT_TIMESTAMP),
+       ('b4c7b393-2e26-49e2-9783-785583bd4c66', '82d58040-8aa9-11ed-a1eb-0242ac120002', '313d9397-b6f8-452d-9f01-5e87004c43fb', 'Product6', 'Description6', false, true, CURRENT_TIMESTAMP),
+       ('c026f325-ebe7-43ea-8e02-f4df7fe6fe7b', '82d58040-8aa9-11ed-a1eb-0242ac120002', 'a675ee5e-57da-423b-9a69-dcd7a40a77af', 'Product7', 'Description2', false, true, CURRENT_TIMESTAMP),
+       ('be2d4c14-e845-412a-b4f7-2986015a5fcd', 'f1bfd355-4bbe-48a4-9acd-c5e886f49277', '97ccb8f6-1f17-450e-9c77-f1f123b0d849', 'Product8', 'Description3', false, true, CURRENT_TIMESTAMP),
+       ('208cd940-df14-4bc0-88eb-66524b213c92', 'fb909c3b-8f5a-4a54-be79-f9922bfdf1c8', 'bad729e9-10e8-4fc6-96c6-6e0f82b781e6', 'Product9', 'Description4', false, true, CURRENT_TIMESTAMP),
+       ('da913318-cfd5-4d04-b11f-170882903d29', 'f93af257-b1c2-4643-a844-5b2a7d75873f', '4dfd650a-b331-4076-8e26-66d9901c13fb', 'Product10', 'Description5', false, true, CURRENT_TIMESTAMP),
+       ('8573d77a-0b3f-4d43-943d-f91e03c033e4', 'e78a56e4-6b3d-40e8-9eeb-4eceac8fc047', 'be0ebe75-d722-4070-beaa-3f3ba1e3bf3d', 'Kremowki', 'Pyszne kremowki', false, true, CURRENT_TIMESTAMP);
+
+INSERT INTO PROPERTIES (PRODUCT_PRODUCT_ID, VALUE, KEY)
+VALUES ('4811913c-b953-4856-979b-838488049d07', 'value1', 'Property1'),
+       ('da913318-cfd5-4d04-b11f-170882903d29', 'value13', 'Property1'),
+       ('4811913c-b953-4856-979b-838488049d07', 'value2', 'Property2'),
+       ('4811913c-b953-4856-979b-838488049d07', 'value3', 'Property3');
+
+INSERT INTO OPINION (OPINION_ID, PRODUCT_ID, DESCRIPTION, RATE, AUTHOR_ID)
+VALUES ('6c3a61be-955c-411b-9942-e746cfd0e75b', '4811913c-b953-4856-979b-838488049d07', 'Test desc 1', 2, '12208864-7b61-4e6e-8573-53863bd93b35'), -- created by user1
+       ('dc0dac8a-797b-11ed-a1eb-0242ac120002', '4811913c-b953-4856-979b-838488049d07', 'desc 2', 3, '66208864-7b61-4e6e-8573-53863bd93b35'), -- created by user
+       ('6ddee1ee-9eba-4222-a031-463a849e1886', '8573d77a-0b3f-4d43-943d-f91e03c033e4', 'Pyszne kremówki najlepsze jakie w życiu jadłem', 5, '6ddee1ee-9eba-4222-a031-463a849e1886');
+
+INSERT INTO ADVANTAGE (ID, VALUE, OPINION_OPINION_ID, OPINION_PRODUCT_ID)
+VALUES ('29e8cfb6-7995-11ed-a1eb-0242ac120002', 'Lorem Ipsum', '6c3a61be-955c-411b-9942-e746cfd0e75b', '4811913c-b953-4856-979b-838488049d07'),
+       ('37fbe192-7995-11ed-a1eb-0242ac120002' ,'Lorem ipsum dolor sit amet', '6c3a61be-955c-411b-9942-e746cfd0e75b', '4811913c-b953-4856-979b-838488049d07');
+
+INSERT INTO DISADVANTAGE (ID, VALUE, OPINION_OPINION_ID, OPINION_PRODUCT_ID)
+VALUES ('43ddbb7a-7995-11ed-a1eb-0242ac120002', 'Lorem Ipsum', '6c3a61be-955c-411b-9942-e746cfd0e75b', '4811913c-b953-4856-979b-838488049d07');
+
+
+INSERT INTO REACTION (AUTHOR_ID, PRODUCT_ID, OPINION_ID, POSITIVE)
+VALUES ('22208864-7b61-4e6e-8573-53863bd93b35', '4811913c-b953-4856-979b-838488049d07', '6c3a61be-955c-411b-9942-e746cfd0e75b', TRUE), -- user2
+       ('62308864-7b61-4e6e-8573-53863bd93b35', '4811913c-b953-4856-979b-838488049d07', '6c3a61be-955c-411b-9942-e746cfd0e75b', FALSE), -- user10
+       ('62808864-7b61-4e6e-8573-53863bd93b35', '4811913c-b953-4856-979b-838488049d07', '6c3a61be-955c-411b-9942-e746cfd0e75b', TRUE); -- user15
+
+INSERT INTO iodb.public.event (dtype, eventid, description, status, userid, opinionid, productid)
+VALUES ('OpinionReportEvent', 'e8a5b357-db44-4a9a-8118-a35448145e2c', '🐶', 0, '66208864-7b61-4e6e-8573-53863bd93b35',
+        '6c3a61be-955c-411b-9942-e746cfd0e75b', '4811913c-b953-4856-979b-838488049d07'),
+       ('OpinionReportEvent', '455db3e7-abfd-4d78-9eb4-399625c88959', '😺', 1, '66208864-7b61-4e6e-8573-53863bd93b35',
+        'dc0dac8a-797b-11ed-a1eb-0242ac120002', '4811913c-b953-4856-979b-838488049d07'),
+       ('OpinionReportEvent', 'f238c3bd-1b75-43b2-b89e-a47f3fa103cb', 'OpinionReport: TEST', 0, '32208864-7b61-4e6e-8573-53863bd93b35',
+        '6ddee1ee-9eba-4222-a031-463a849e1886', '8573d77a-0b3f-4d43-943d-f91e03c033e4');
+INSERT INTO iodb.public.event (dtype, eventid, description, status, userid, questionid)
+VALUES ('QuestionNotifyEvent', '1991023d-8ee5-41fb-b5e3-4076ea49417b', 'New question!!!', 0, '32208864-7b61-4e6e-8573-53863bd93b35',
+        '8d2f2aaf-1b98-4d87-b7e3-97bacd955bc6');
+INSERT INTO iodb.public.event (dtype, eventid, description, status, userid, productid)
+VALUES ('ProductReportEvent', '1bc29679-fc39-4a1d-b4b7-0f45b45690ed', 'Produkt zbyt kremowy', 0, '32208864-7b61-4e6e-8573-53863bd93b35',
+        '8573d77a-0b3f-4d43-943d-f91e03c033e4');
+INSERT INTO iodb.public.event (dtype, eventid, description, status, userid, questionid)
+VALUES ('AnswerReportEvent', 'b12ac4f9-9e79-4420-987e-039d3f64bc56', 'Not Good Answer', 0, '82208864-7b61-4e6e-8573-53863bd93b35',
+        'd60e9ea8-129f-4b8f-b9ab-5cdcd6aee228');
+INSERT INTO iodb.public.event (dtype, eventid, description, status, userid, questionid)
+VALUES ('QuestionReportEvent', 'd60e9ea8-129f-4b8f-b9ab-5cdcd6aee228', 'Not Good Answer', 0, '62908864-7b61-4e6e-8573-53863bd93b35',
+        '0338ce3f-b8d3-4cf2-b814-b7ddffc17632');
